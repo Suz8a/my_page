ad270f467cff290593501fd807fc887f2e348671
Suz8a <lmartinez.bno@gmail.com>
2020-01-21 14:25:15 -0700

    centering information

--- presentation-page/src/components/About.vue
+++ presentation-page/src/components/About.vue
@@ -2,15 +2,14 @@
   <sui-grid class="about">
     <sui-grid-row>
     <sui-grid-column/>
-      <sui-grid-column :width="10">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      <sui-grid-column  :width="10">
+      <div class="information">
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
         when an unknown printer took a galley of type and scrambled it to make a type 
         specimen book. It has survived not only five centuries, but also the leap into 
-        electronic typesetting, remaining essentially unchanged. It was popularised in 
-        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including
-         versions of Lorem Ipsum.
+        electronic typesetting, remaining essentially unchanged. It was popularised 
+      </div>
       </sui-grid-column>
       <sui-grid-column/>
     </sui-grid-row>
@@ -31,7 +30,10 @@ export default {
     width:100vw;
     height: 100vh;
     background-color:#221f1e;
-    padding: auto;
+}
+
+.information{
+    margin: 25% 0 25% 0;
 }
 
 </style>
