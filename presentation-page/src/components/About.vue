9b25201908a225ca0ad9bd71493cb8ba6ae1a706
Suz8a <lmartinez.bno@gmail.com>
2020-01-21 13:28:22 -0700

    Three sections done

---
+++ presentation-page/src/components/About.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <sui-grid class="about">
+    <sui-grid-row>
+    <sui-grid-column/>
+      <sui-grid-column :width="10">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type 
+        specimen book. It has survived not only five centuries, but also the leap into 
+        electronic typesetting, remaining essentially unchanged. It was popularised in 
+        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including
+         versions of Lorem Ipsum.
+      </sui-grid-column>
+      <sui-grid-column/>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+    name:'about'   
+}
+</script>
+
+<style>
+.about{
+    color: white;
+    font-size: 20px;
+    line-height: 36px;
+    width:100vw;
+    height: 100vh;
+    background-color:#221f1e;
+    padding: auto;
+}
+
+</style>
---
+++ presentation-page/src/components/Experience.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <sui-grid class="experience">
+    <sui-grid-row>
+    <sui-grid-column/>
+      <sui-grid-column :width="10">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type 
+        specimen book. It has survived not only five centuries, but also the leap into 
+        electronic typesetting, remaining essentially unchanged. It was popularised in 
+        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including
+         versions of Lorem Ipsum.
+      </sui-grid-column>
+      <sui-grid-column/>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+    name:'experience'   
+}
+</script>
+
+<style>
+.experience{
+    color: white;
+    width: 100vw;
+    height: 100vh;
+    background-color: #221f1e;
+}
+
+</style>
