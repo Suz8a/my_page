1096b5415000aac1961fcd24270554a8072b736d
Suz8a <lmartinez.bno@gmail.com>
2020-01-23 13:48:57 -0700

    skills & animation added

--- presentation-page/src/components/About.vue
+++ presentation-page/src/components/About.vue
@@ -1,40 +1,32 @@
 <template lang="html">
   <sui-grid class="about">
-  <sui-grid-row/>
-  <sui-grid-row/>
-  <sui-grid-row/>
-    <sui-grid-row>
-      <img class="profilePhoto fade-in" src="../assets/profile.jpg" />
+    <img class="background-image" src="https://cdn.pixabay.com/photo/2015/10/27/14/40/programming-1009134_960_720.jpg">
+  <sui-grid-row v-for="n in 3" :key="n" />
+  <sui-grid-row>
+      <img class="profilePhoto in-left" src="../assets/profile.jpg" />
   </sui-grid-row>
   <sui-grid-row/>
   <sui-grid-row>
     <sui-grid-column :width="2" />
       <sui-grid-column  :width="8">
-        <div class="title">About Me</div>
+        <div class="title in-top">About Me</div>
       </sui-grid-column>
   </sui-grid-row>
     <sui-grid-row>
-    <sui-grid-column :width="2" />
-      <sui-grid-column  :width="6">
-      <div class="information">
+      <sui-grid-column :width="2" />
+        <sui-grid-column  :width="6">
+          <div class="information in-top-info">
          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
         when an unknown printer took a galley of type and scrambled it to make a type 
         specimen book. It has survived not only five centuries, but also the leap into 
         electronic typesetting, remaining essentially unchanged. It was popularised 
         
-      </div>
-      
-      </sui-grid-column>
+          </div>
+        </sui-grid-column>
       <sui-grid-column :width="8" />
     </sui-grid-row>
-    <sui-grid-row/>
-    <sui-grid-row/>
-    <sui-grid-row/>
-    <sui-grid-row/>
-    <sui-grid-row/>
-    <sui-grid-row/>
-    
+    <sui-grid-row v-for="n in 6" :key="n" />
   </sui-grid>
 </template>
 
@@ -42,27 +34,55 @@
 export default {
     name:'about'   
 }
+
 </script>
 
-<style>
-.fade-in {
-  animation: fadeIn ease 2.5s;
-  -webkit-animation: fadeIn ease 2.5s;
-  -moz-animation: fadeIn ease 2.5s;
-  -o-animation: fadeIn ease 2.5s;
-  -ms-animation: fadeIn ease 2.5s;
+<style scope>
+@keyframes fadeInRight {
+    from {
+        opacity:0;
+        transform: translatex(100px);
+    }
+    to {
+        opacity:1;
+        transform: translatex(0);
+    }
 }
-
-
-@keyframes fadeIn{
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
+@keyframes fadeInTop {
+    from {
+        opacity:0;
+        transform: translatey(100px);
+    }
+    to {
+        opacity:1;
+        transform: translatey(0);
+    }
 }
-
+.in-top{
+    animation-name: fadeInTop;
+    animation-fill-mode: both;
+    animation-duration: 0.6s;
+    animation-delay: 0.2s;
+}
+.in-top-info{
+    animation-name: fadeInTop;
+    animation-fill-mode: both;
+    animation-duration: 0.6s;
+    animation-delay: 0.5s;
+}
+.in-left {
+    animation-name: fadeInRight;
+    animation-fill-mode: both;
+    animation-duration: 0.6s;
+    animation-delay: 0.8s;
+}
+.background-image{
+  position: absolute;
+  width: 100%;
+  height:100%;
+  opacity: 0.04;
+  
+}
 .profilePhoto{
   position: absolute;
   left: 55%;
@@ -77,6 +97,9 @@ export default {
   font-size: 50px;
 }
 .about{
+    height: 100vh;
+    width: 100vw;
+    text-align: justify;
     color: white;
     font-size: 20px;
     line-height: 30px;
--- presentation-page/src/components/Skill.vue
+++ presentation-page/src/components/Skill.vue
@@ -1,19 +1,50 @@
 <template lang="html">
   <sui-grid class="skill">
-    <sui-grid-row>
-    <sui-grid-column/>
-      <sui-grid-column :width="10">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type 
-        specimen book. It has survived not only five centuries, but also the leap into 
-        electronic typesetting, remaining essentially unchanged. It was popularised in 
-        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including
-         versions of Lorem Ipsum.
+  <img class="background-image-skills" src="../assets/blackWallpaper.jpg">
+  <sui-grid-row/>
+    <sui-grid-column>
+      <sui-grid-row>
+        <h1 class="skill-title">Skills</h1>
+      </sui-grid-row>
+    </sui-grid-column>
+    <sui-grid-row :columns="4">
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/js.png">
+        </div>
+      </sui-grid-column>
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/html5.png">
+        </div>
+      </sui-grid-column>
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/css.jpg">
+        </div>
+      </sui-grid-column>
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/react.jpg">
+        </div>
+      </sui-grid-column>
+    </sui-grid-row>
+    <sui-grid-row />
+    <sui-grid-row :columns="4">
+      <sui-grid-column/>
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/vue.png">
+        </div>
+      </sui-grid-column>
+      <sui-grid-column>
+        <div class="image-container">
+          <img class="image" src="../assets/mongoDB.png">
+        </div>
       </sui-grid-column>
       <sui-grid-column/>
     </sui-grid-row>
+    <sui-grid-row/>
   </sui-grid>
 </template>
 
@@ -23,12 +54,33 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
+.background-image-skills{
+  opacity: 0.2;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.skill-title{
+  padding-left: 20px;
+}
 .skill{
     color: white;
     width: 100vw;
     height: 100vh;
     background-color: #292625;
+}
+.image{
+  width: 150px;
+  height: 150px;
+}
+.image-container{
+  margin: 0 auto;
+  padding: 50px;
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  background-color: white;
 }
 
 </style>
